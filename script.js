bafd8c203b7fce66d5b91211b34342a80171ccc2
SAKTHI-VT <sakthivelthirumoorthy2002@gmail.com>
2023-01-28 10:57:22 +0530

    commit

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 function myFunction() {
     var x = document.getElementById("myInput");
+    var y = document.getElementById("myInput1");
     if (x.type === "password") {
         x.type = "text";
     } 
     else {
         x.type = "password";
+    }
+    if(y.type === "password"){
+        y.type="text";
+    }
+    else{
+        y.type = "password";
     }
 }
 // function myfunction(){
