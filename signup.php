b53ee6a0379b68b27b0017b6aa580256d61b053d
SAKTHI-VT <sakthivelthirumoorthy2002@gmail.com>
2023-03-10 17:35:09 +0530

    commit

---
+++ signup.php
@@ -0,0 +1,56 @@
+<?php
+
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$email  = $_POST['email'];
+$pass1 = $_POST['pass1'];
+$pass2 = $_POST['pass2'];
+
+
+if (!empty($fname) || !empty($lname) || !empty($email) || !empty($pass1) || !empty($pass2) )
+{
+
+$host = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "project";
+
+
+
+// Create connection
+$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+
+if (mysqli_connect_error()){
+  die('Connect Error ('. mysqli_connect_errno() .') '
+    . mysqli_connect_error());
+}
+else{
+  $SELECT = "SELECT email From register Where email = ? Limit 1";
+  $INSERT = "INSERT Into register (fname , lname, email ,pass1, pass2 )values(?,?,?,?)";
+
+//Prepare statement
+     $stmt = $conn->prepare($SELECT);
+     $stmt->bind_param("s", $email);
+     $stmt->execute();
+     $stmt->bind_result($email);
+     $stmt->store_result();
+     $rnum = $stmt->num_rows;
+
+     //checking username
+      if ($rnum==0) {
+      $stmt->close();
+      $stmt = $conn->prepare($INSERT);
+      $stmt->bind_param("sssss", $fname,$lname,$email,$pass1,$pass2);
+      $stmt->execute();
+      echo "New record inserted sucessfully";
+     } else {
+      echo "Someone already register using this email";
+     }
+     $stmt->close();
+     $conn->close();
+    }
+} else {
+ echo "All field are required";
+ die();
+}
+?>
